9f47fdab2cb1bf7fc10eac8abb1f4a49dc88feb9
Lian Arthofer <77974438+LianArthofer@users.noreply.github.com>
2023-10-23 16:50:25 +0100

    dawdaw

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,38 +3,23 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `lianarthofer.github.io`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://lianarthofer.github.io/`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    {
-      resolve: `gatsby-plugin-google-gtag`,
+    { // this must be loaded first in order to work
+      resolve: `gatsby-plugin-gtag`, // note this instead of gatsby-plugin-react-helmet
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-F804S3C6B9", // Google Analytics / GA
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          anonymize_ip: true,
-          cookie_expires: 0,
-          send_page_view: true // default appears to be false.
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-        },
-      },
+        trackingId: "G-F804S3C6B9",
+        head: true, // note this is TRUE and not FALSE as listed in other examples above
+        anonymize: true
+      }
     },
+    "gatsby-plugin-react-helmet",
+    // other plugins
   ]
 };
 
