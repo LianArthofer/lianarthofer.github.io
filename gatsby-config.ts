eaf9f220ea93ffc1b11b43c8fca83ee4a134eef3
Lian Arthofer <77974438+LianArthofer@users.noreply.github.com>
2023-10-23 16:32:07 +0100

    blud

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,16 +10,31 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    { // this must be loaded first in order to work
-      resolve: `gatsby-plugin-gtag`, // note this instead of gatsby-plugin-react-helmet
+    {
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "G-F804S3C6B9",
-        head: true, // note this is TRUE and not FALSE as listed in other examples above
-        anonymize: true
-      }
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-F804S3C6B9", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+          send_page_view: true // default appears to be false.
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
     },
-    "gatsby-plugin-react-helmet",
-    // other plugins
   ]
 };
 
